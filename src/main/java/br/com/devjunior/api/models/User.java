18a111f6831dc551c598e69b7b92e3b2961912c0
Italo Santiago <italosantiagoo22@gmail.com>
2022-08-08 08:16:52 -0300

    User class implementation

---
+++ src/main/java/br/com/devjunior/api/models/User.java
@@ -0,0 +1,25 @@
+package br.com.devjunior.api.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+}
