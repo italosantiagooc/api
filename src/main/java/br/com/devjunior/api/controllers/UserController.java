09f578be3b55908ed147126d90f0bc86a5ab5e02
Italo Santiago <italosantiagoo22@gmail.com>
2022-08-09 08:36:15 -0300

    update findById

--- src/main/java/br/com/devjunior/api/controllers/UserController.java
+++ src/main/java/br/com/devjunior/api/controllers/UserController.java
@@ -1,6 +1,7 @@
 package br.com.devjunior.api.controllers;
 
 import br.com.devjunior.api.models.User;
+import br.com.devjunior.api.services.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,10 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/users")
 public class UserController {
 
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id) {
-        return ResponseEntity.ok()
-                        .body(new User( 1, "Italo da Silva Santiago", "italosantiagoo22@gmail.com", "italo10"));
-
+        return ResponseEntity.ok().body(userService.findById(id));
     }
 }
