306bb7b6b55c4fac94e536429e099513f0010a28
Italo Santiago <italosantiagoo22@gmail.com>
2022-08-09 09:33:27 -0300

    Endpoint findAll

--- src/main/java/br/com/devjunior/api/controllers/UserController.java
+++ src/main/java/br/com/devjunior/api/controllers/UserController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
@@ -26,5 +29,11 @@ public class UserController {
     @GetMapping(value = "/{id}")
     public ResponseEntity<UserDto> findById(@PathVariable Integer id) {
         return ResponseEntity.ok().body(modelMapper.map(userService.findById(id), UserDto.class));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDto>> findAll() {
+        return ResponseEntity.ok().body(userService.findAll().stream().map(x -> modelMapper.map(x, UserDto.class))
+                .collect(Collectors.toList()));
     }
 }
